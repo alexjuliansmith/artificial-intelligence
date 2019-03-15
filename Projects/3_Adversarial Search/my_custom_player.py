dc0c5675d5a5d7b92dacf90b558d0132211e7036
alexjuliansmith <alexjuliansmith@gmail.com>
2019-03-15 15:01:34 +0000

    Feature:  Add function to find set of knight moves from bitboard

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,8 +1,189 @@
 
-from sample_players import DataPlayer
+import random
 
+#from isolation import *
+import isolation.isolation as isolation
+
+from sample_players import DataPlayer, MinimaxPlayer
+
+
+
+WIN, LOSS = float("inf"), float("-inf")
+
+MOVE_BIT_SHIFTS = [int(a) for a in isolation._ACTIONSET if a > 0]
+BOARD_MASK = isolation._BLANK_BOARD
+
+######################################
+# Bitboard functions
+#####################################
+
+def count_bits(integer):
+    '''
+    Kernagham algorithm to count number of set bits in an integer
+    Can be used to find the number of flagged squares in a bitboard representation
+    For instance, in the bitboard representation of the game state it will count the number of remaining free squares
+    :param integer:
+    :return: number of set bits in integer
+    '''
+    bit_count = 0
+    while integer:
+        integer &= (integer - 1)
+        bit_count += 1
+    return bit_count
+
+def get_knight_moves(knights, valid_squares):
+    """
+    Given
+    knights: bitboard with locations of a set of knights
+    valid_squares: bitboard identifying all squares that can be legally moved to
+    Return
+    Bitboard with all valid squares that any knight can move to (doesn't include original knight positions)
+    """
+    moves = 0
+    for mbs in MOVE_BIT_SHIFTS:
+        moves |= (knights << mbs)
+        moves |= (knights >> mbs)
+    moves &= valid_squares
+    return moves
+
+
+
+######################################
+# Bitboard functions
+#####################################
+
+######################################
+# Custom Players
+#####################################
+
+class IterativeDeepeningPlayer(DataPlayer):
+
+    score = MinimaxPlayer.score  # Default heuristic, can be overridden in subclass
+
+    def get_action(self, state):
+
+        try:
+            # Seed the queue, so we always have a move after timeout
+            self.queue.put(random.choice(state.actions()))
+        except IndexError:
+            # Active Player has no legal moves, just return leaving the queue empty
+            return
+
+        # If this is not player's first move, progressively improve the seed move using an iterative deepening search
+        if state.ply_count >= 2:
+            # Continue iterative deepening until we run out of free aquares (or time)
+            free_squares = count_bits(state.board)
+            for depth in range(free_squares):
+                best_score, best_move = self.search(state, depth + 1)
+                self.queue.put(best_move)
+                if best_score in (WIN, LOSS):
+                    break  # Forced result, no need to use up the rest of the search time
+
+    def search(self, state, depth):
+        '''
+        Abstract method to be implemented in subclasses
+        :param state: Game State
+        :param depth: Requested search move depth
+        :return: Pair (best_score, best_move)
+        representing the best action found and its (heuristic or game theoretic) score
+        (If subclasses only want to return an action, they can return None for best_score)
+        '''
+        raise NotImplementedError
+
+class AlphaBetaPlayer(IterativeDeepeningPlayer):
+
+    def search(self, state, depth):
+        return self.max_value(state, depth, LOSS, WIN)
+
+    def min_value(self, state, depth, alpha, beta):
+
+        moves = state.actions()
+        if not moves:
+            return WIN, None  # No available moves, opponent has lost
+        if not state._has_liberties(self.player_id):
+            return LOSS, moves[0]  # Active player has no moves left, any opponent move wins
+        if depth <= 0:
+            return self.score(state), None  # Leaf search node, evaluate position instead of moving
+
+        # Find best move for opponent
+        best_score, best_move = WIN, moves[0]
+        for move in moves:
+            score, _ = self.max_value(state.result(move), depth - 1, alpha, beta)
+            if score < best_score:
+                if score <= alpha:
+                    return score, move  # Maximising player has an equal or better move choice, stop searching this line
+                best_score, best_move = score, move
+                beta = min(beta, score)
+
+        return best_score, best_move
+
+
+    def max_value(self, state, depth, alpha, beta):
+
+        moves = state.actions()
+        if not moves:
+            return LOSS, None  # No available moves, active player has lost
+        if not state._has_liberties(1 - self.player_id):
+            return WIN, moves[0]  # Opponent has no moves left, any active player move wins
+        if depth <= 0:
+            return self.score(state), None  # Leaf search node, evaluate position instead of moving
+
+        # Find best move for active player
+        best_score, best_move = LOSS, moves[0]
+        for move in moves:
+            score, _ = self.min_value(state.result(move), depth-1, alpha, beta)
+            if score > best_score:
+                if score >= beta:
+                    return score, move  # Minimising player has an equal or better move choice, stop searching this line
+                best_score, best_move = score, move
+                alpha = max(alpha, score)
+
+        return best_score, best_move
+
+class ID_MinimaxPlayer(IterativeDeepeningPlayer):
+
+    def search(self, state, depth):
+        def min_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1))
+            return value
+
+        def max_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1))
+            return value
+
+        results = [(min_value(state.result(action), depth - 1), action) for action in state.actions()]
+        return max(results)
+
+class VerifyEquivalencePlayer(AlphaBetaPlayer, ID_MinimaxPlayer):
+
+    def search(self, state, depth):
+        ab_score, ab_move = AlphaBetaPlayer.search(self, state, depth)
+        mm_score, mm_move = ID_MinimaxPlayer.search(self, state, depth)
+
+        if ab_score != mm_score:
+            print("AB Score {} Move {}\nMM Score {} Move {}".format(ab_score, ab_move, mm_score, mm_move))
+            debug = DebugState.from_state(state)
+            print("Active Player: ", debug.player_symbols[self.player_id])
+            print("Ply: ", state.ply_count)
+            print("Search Depth: ", depth)
+            print(debug)
+
+        return ab_score, ab_move or mm_move
+
+######################################
+# Custom Players
+#####################################
 
 class CustomPlayer(DataPlayer):
+
     """ Implement your own agent to play knight's Isolation
 
     The get_action() method is the only required method for this project.
@@ -45,8 +226,6 @@ class CustomPlayer(DataPlayer):
         ##import random
         ##self.queue.put(random.choice(state.actions()))
 
-### TO TEMP imports - remove before submission
-from my_sample_players import *
 
 CustomPlayer = AlphaBetaPlayer
 CustomPlayer = ID_MinimaxPlayer
---
+++ Projects/3_Adversarial Search/tests/test_my_heuristic.py
@@ -0,0 +1,38 @@
+
+import unittest
+
+from collections import deque
+from random import choice
+from textwrap import dedent
+
+from isolation import Isolation, Agent, fork_get_action, play
+from sample_players import RandomPlayer
+from my_custom_player import *
+
+
+
+
+class BaseCustomHeuristicTest(unittest.TestCase):
+    def setUp(self):
+        self.MID = 57   #Assuming 11 by 9 board
+        self.knight_start  = 1 << self.MID
+
+
+
+class CustomPlayerGetActionTest(BaseCustomHeuristicTest):
+
+    def test_get_knights(self):
+        """ get_action() calls self.queue.put() before timeout on an empty board """
+        knights = self.knight_start
+        booard_ids = [41523161179755588175928227572738047,
+                      38114874426268006157864658273658711,
+                      13835983132169903245103409681572522,
+                      27687178071769218837580222542726485,
+                      13835983132169903245103409681572522]
+
+        for i in range(5):
+            moves = get_knight_moves(knights, isolation._BLANK_BOARD)
+            photo_negative = moves ^ BOARD_MASK
+            self.assertEqual(photo_negative, booard_ids[i])
+
+            knights = moves
